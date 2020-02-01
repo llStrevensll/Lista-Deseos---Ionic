a29e3c83133bc0a83d5a1dd66f5967ce1eed1c0d
Steven <anguelr12@hotmail.com>
2020-02-01 10:27:47 -0500

    pantalla pendientes, modificacion de colores

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -10,7 +10,15 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-    console.log('Servicio inicializado');
+    //Listas
+    const lista1  = new Lista('Objetivos a Realizar');
+    const lista2 = new Lista('Tareas Universidad');
+
+    //Insertar listas en el arreglo
+    this.listas.push(lista1,lista2);
+
+    
     
    }
 }
+ 
