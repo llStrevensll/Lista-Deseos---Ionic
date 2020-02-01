3573226892345d769f1a057754cc46b85d0aa621
Steven <anguelr12@hotmail.com>
2020-02-01 11:52:39 -0500

    LocalStorage - persistencia en la info

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -10,12 +10,15 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
+
+    this.cargarStorage();
+
     //Listas
-    const lista1  = new Lista('Objetivos a Realizar');
-    const lista2 = new Lista('Tareas Universidad');
+    //const lista1  = new Lista('Objetivos a Realizar');
+    //const lista2 = new Lista('Tareas Universidad');
 
     //Insertar listas en el arreglo
-    this.listas.push(lista1,lista2);
+    //this.listas.push(lista1,lista2);
    }
 
    //Crear Lista
@@ -23,6 +26,21 @@ export class DeseosService {
 
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+   }
+
+   //Storage
+   guardarStorage(){//JSON.stringify-> pasar a string
+      localStorage.setItem('data', JSON.stringify(this.listas));
+   } 
+
+   cargarStorage(){
+      if (localStorage.getItem('data')) {//si existe
+        this.listas = JSON.parse(localStorage.getItem('data'));
+      }else {
+        this.listas = [];
+      }
+    
    }
 }
  
