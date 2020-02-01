8674b9ce12ed07c45d982df7dc80fb4e3f4d3076
Steven <anguelr12@hotmail.com>
2020-02-01 17:24:13 -0500

    editar titulo de la lista

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
 import { Lista } from '../../models/lista.model';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -9,11 +10,16 @@ import { Lista } from '../../models/lista.model';
   styleUrls: ['./listas.component.scss'],
 })
 export class ListasComponent implements OnInit {
-  
+
+  //ViewChild 
+  //@ViewChild( IonList ) lista: IonList;
+  @ViewChild( IonList, null) lista: IonList;
   //Para saber si esta en terminados o en pendientes
   @Input() terminada = true;
-
-  constructor(public deseosService: DeseosService, private router: Router) { }
+  
+  
+  
+  constructor(public deseosService: DeseosService, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {}
   
@@ -26,6 +32,48 @@ export class ListasComponent implements OnInit {
     }
 
   }
+  //Editar lista
+  async editarLista(lista){
+
+      const alert = await this.alertController.create({
+        header: 'Editar Lista',
+        inputs: [
+          {
+          name: 'titulo',
+          type: 'text',
+          value: lista.titulo,
+          placeholder: 'Nombre de la lista'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancelar');   
+              this.lista.closeSlidingItems();
+            }
+          },
+          {
+            text: 'Actualizar',
+            handler: (data) => {
+              console.log(data);
+              if (data.titulo.lenght === 0) {
+                return;
+              }
+              
+              lista.titulo = data.titulo;
+              this.deseosService.guardarStorage();
+              this.lista.closeSlidingItems();
+            }
+          }
+        ]
+      });
+  
+      alert.present();
+  }
+
+
    //borrarLista
   borrarLista( lista: Lista){
       
