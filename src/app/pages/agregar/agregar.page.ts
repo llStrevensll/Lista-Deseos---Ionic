6b087168423969b31a63c6f3cf9d0a168b9107bc
Steven <anguelr12@hotmail.com>
2020-02-01 15:40:23 -0500

    animated, mantener cambio en checkbox

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -41,5 +41,27 @@ export class AgregarPage implements OnInit {
     this.nombreItem = '';
     this.deseosService.guardarStorage();
   }
+  
+  //Cambio del checkBox
+  cambioCheck( item:ListaItem){
+    const pendientes = this.lista.items
+                            .filter(itemData => !itemData.completado)
+                            .length;
+    
+    if (pendientes === 0) {
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    }else {
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+    }
+                            
+
+
+    this.deseosService.guardarStorage();
+    console.log(this.deseosService.listas);
+    
+    
+  }
 
 }
